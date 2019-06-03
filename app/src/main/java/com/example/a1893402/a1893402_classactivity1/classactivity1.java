f663595519502ef51961bed99417ffade27a0561
narendergayam <narendarreddyg16@gmail.com>
2019-06-03 14:34:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1893402/a1893402_classactivity1/classactivity1.java
@@ -0,0 +1,13 @@
+package com.example.a1893402.a1893402_classactivity1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class classactivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classactivity1);
+    }
+}
